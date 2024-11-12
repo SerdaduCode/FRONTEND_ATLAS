2bd2e656588f94afc31869fd3e9af35f2306723c
Wildan <wildanfajriyann@gmail.com>
2024-11-12 08:01:12 +0700

    refactor dummy data

--- src/data/data.ts
+++ src/data/data.ts
@@ -1,189 +1,412 @@
 export const provinsi = [
   ['id-3700'],
-  ['id-ac', undefined],
-  ['id-jt', undefined],
-  ['id-be', undefined],
-  ['id-bt', undefined],
-  ['id-kb', undefined],
-  ['id-bb', undefined],
-  ['id-ba', undefined],
-  ['id-ji', undefined],
-  ['id-ks', undefined],
-  ['id-nt', undefined],
-  ['id-se', undefined],
-  ['id-kr', undefined],
-  ['id-ib', undefined],
-  ['id-su', undefined],
-  ['id-ri', undefined],
-  ['id-sw', undefined],
-  ['id-ku', undefined],
-  ['id-la', undefined],
-  ['id-sb', undefined],
-  ['id-ma', undefined],
-  ['id-nb', undefined],
-  ['id-sg', undefined],
-  ['id-st', undefined],
-  ['id-pa', undefined],
-  ['id-jr', undefined],
-  ['id-ki', undefined],
-  ['id-1024', undefined],
-  ['id-jk', undefined],
-  ['id-go', undefined],
-  ['id-yo', undefined],
-  ['id-sl', undefined],
-  ['id-sr', undefined],
-  ['id-ja', undefined],
-  ['id-kt', undefined],
+  ['id-jt', undefined], // aceh # 1
+  ['id-ac', undefined], // jawa tengah # 2
+  ['id-be', undefined], // bengkulu # 3
+  ['id-bt', undefined], // banten # 4
+  ['id-kb', undefined], // kalimantan barat # 5
+  ['id-bb', undefined], // bangka belitung # 6
+  ['id-ba', undefined], // bali # 7
+  ['id-ji', undefined], // jawa timur # 8
+  ['id-ks', undefined], // kalimantan selatan # 9
+  ['id-nt', undefined], // ntt # 10
+  ['id-se', undefined], // sulawesi selatan # 11
+  ['id-kr', undefined], // kepulauan riau # 12
+  ['id-ib', undefined], // irian jaya barat # 13
+  ['id-su', undefined], // sumatera utara # 14
+  ['id-ri', undefined], // riau # 15
+  ['id-sw', undefined], // sulawesi utara # 16
+  ['id-ku', undefined], // kalimantan utara # 17
+  ['id-la', undefined], // maluku utara # 18
+  ['id-sb', undefined], // sumatera barat # 19
+  ['id-ma', undefined], // maluku # 20
+  ['id-sg', undefined], // sulawesi tenggara # 22
+  ['id-nb', undefined], // ntb # 21
+  ['id-st', undefined], // sulawesi tengah # 23
+  ['id-pa', undefined], // papua # 24
+  ['id-jr', undefined], // jawa barat # 25
+  ['id-ki', undefined], // kalimantan timur # 26
+  ['id-1024', undefined], // lampung # 27
+  ['id-jk', undefined], // jakarta raya # 28
+  ['id-go', undefined], // gorontalo # 29
+  ['id-yo', undefined], // yogyakarta # 30
+  ['id-sl', undefined], // sumatera selatan # 31
+  ['id-sr', undefined], // sulawesi barat # 32
+  ['id-ja', undefined], // jambi # 33
+  ['id-kt', undefined], // kalimantan tengah # 34
 ];
 
-export const kota = [
-  {
-    id: 1,
-    name: 'Bandung',
-    lat: -6.914744,
-    lon: 107.60981,
-    nickname: 'Kota Kembang',
-    // image:
-    //   'https://cdn-assetd.kompas.id/9D-E_Du8a4wsbIeRlvEXRkeEkuk=/1024x683/https%3A%2F%2Fkompas.id%2Fwp-content%2Fuploads%2F2019%2F09%2F20190930SEM3-BAGIAN-ATAP-GEDUNG-SATE_1569844378.jpg',
-    image:
-      'https://asset.kompas.com/crops/fXl7uewsJWP3O3cGure7FoZWwCE=/3x0:744x494/1200x800/data/photo/2020/11/20/5fb727a581578.png',
-  },
-  {
-    id: 2,
-    name: 'Denpasar',
-    lat: -8.65,
-    lon: 115.216667,
-    nickname: 'Kota Parijis Van Bally',
-    image:
-      'https://upload.wikimedia.org/wikipedia/commons/f/f8/Aerial_view_of_Bajra_Sandhi_Monument_Denpasar_Bali_Indonesia.jpg',
-  },
-  {
-    id: 3,
-    name: 'Makassar',
-    lat: -5.135399,
-    lon: 119.42379,
-    nickname: null,
-    image:
-      'https://upload.wikimedia.org/wikipedia/commons/4/45/Makassar_Sulawesi_Pantai_Losari.jpg',
-  },
-  {
-    id: 4,
-    name: 'Jambi',
-    lat: -1.609972,
-    lon: 103.607254,
-    nickname: null,
-    image:
-      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTriSJ5qWaPvPshH_ebgzmeOBeN6ufCdTca1g&s',
-  },
-  {
-    id: 5,
-    name: 'Ambon',
-    lat: -3.69583,
-    lon: 128.18333,
-    nickname: null,
-    image:
-      'https://www.indonesia.travel/content/dam/indtravelrevamp/id-id/destinasi/ambon/thumbnail.jpg',
-  },
-  {
-    id: 6,
-    name: 'Samarinda',
-    lat: -0.502106,
-    lon: 117.153709,
-    nickname: null,
-    image:
-      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTjL8bGvP4rjn5YJeGKzTyiiioFA9pweMohfQ&s',
-  },
-  {
-    id: 7,
-    name: 'Pontianak',
-    lat: 0.0,
-    lon: 109.333336,
-    nickname: null,
-    image:
-      'https://upload.wikimedia.org/wikipedia/commons/1/1f/Equator_Pontianak_Monument.jpg',
-  },
-  {
-    id: 8,
-    name: 'Medan',
-    lat: 3.597031,
-    lon: 98.678513,
-    nickname: null,
-    image:
-      'https://www.rumah123.com/seo-cms/assets/Medan_Kota_c5625f2988/Medan_Kota_c5625f2988.webp',
-  },
-  {
-    id: 9,
-    name: 'Kupang',
-    lat: -10.178757,
-    lon: 123.597603,
-    nickname: null,
-    image:
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/4/43/Night_at_Kupang_city.jpg/1200px-Night_at_Kupang_city.jpg',
-  },
-  {
-    id: 10,
-    name: 'Jayapura',
-    lat: -2.53371,
-    lon: 140.71813,
-    nickname: null,
-    image:
-      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQqIm1MywLu2mg-Mcn-1bex5-bScXbtFxijPA&s',
-  },
-  {
-    id: 11,
-    name: 'Banjarbaru',
-    lat: -3.457242,
-    lon: 114.810318,
-    nickname: null,
-    image:
-      'https://upload.wikimedia.org/wikipedia/commons/2/2b/Al_Munawarah_Grand_Mosque_Banjarbaru.jpg',
-  },
-  {
-    id: 12,
-    name: 'Palu',
-    lat: -0.8989404,
-    lon: 119.8714135,
-    nickname: null,
-    image:
-      'https://upload.wikimedia.org/wikipedia/commons/1/1c/Palu_city_at_night.png',
-  },
-  {
-    id: 13,
-    name: 'Manado',
-    lat: 1.47483,
-    lon: 124.842079,
-    nickname: null,
-    image:
-      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRqGyrwkfM9Ktc-B1GST6AASOuq4yA1xrRbVw&s',
-  },
-  {
-    id: 14,
-    name: 'Yogyakarta',
-    lat: -7.797068,
-    lon: 110.370529,
-    nickname: 'Kota Pelajar',
-    image:
-      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRYlFPTmiGqBY5YxA4qat0eg05iHHaUp22w1A&s',
-  },
-  {
-    id: 15,
-    name: 'Malang',
-    lat: -7.983908,
-    lon: 112.621391,
-    nickname: null,
-    image:
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/9/96/Tugu_Malang.jpg/288px-Tugu_Malang.jpg',
-  },
-  {
-    id: 16,
-    name: 'Jakarta',
-    lat: -6.2,
-    lon: 106.816666,
-    nickname: 'Kota Metropolitan',
-    image:
-      'https://pict.sindonews.net/dyn/850/pena/news/2021/02/26/173/348284/beragam-nama-jakarta-sejak-tahun-397-sampai-sekarang-txm.jpg',
-  },
-];
+// export const kota = [
+//   {
+//     id: 1,
+//     name: 'Bandung',
+//     lat: -6.914744,
+//     lon: 107.60981,
+//     nickname: 'Kota Kembang',
+//     // image:
+//     //   'https://cdn-assetd.kompas.id/9D-E_Du8a4wsbIeRlvEXRkeEkuk=/1024x683/https%3A%2F%2Fkompas.id%2Fwp-content%2Fuploads%2F2019%2F09%2F20190930SEM3-BAGIAN-ATAP-GEDUNG-SATE_1569844378.jpg',
+//     image:
+//       'https://asset.kompas.com/crops/fXl7uewsJWP3O3cGure7FoZWwCE=/3x0:744x494/1200x800/data/photo/2020/11/20/5fb727a581578.png',
+//   },
+//   {
+//     id: 2,
+//     name: 'Denpasar',
+//     lat: -8.65,
+//     lon: 115.216667,
+//     nickname: 'Kota Parijis Van Bally',
+//     image:
+//       'https://upload.wikimedia.org/wikipedia/commons/f/f8/Aerial_view_of_Bajra_Sandhi_Monument_Denpasar_Bali_Indonesia.jpg',
+//   },
+//   {
+//     id: 3,
+//     name: 'Makassar',
+//     lat: -5.135399,
+//     lon: 119.42379,
+//     nickname: null,
+//     image:
+//       'https://upload.wikimedia.org/wikipedia/commons/4/45/Makassar_Sulawesi_Pantai_Losari.jpg',
+//   },
+//   {
+//     id: 4,
+//     name: 'Jambi',
+//     lat: -1.609972,
+//     lon: 103.607254,
+//     nickname: null,
+//     image:
+//       'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTriSJ5qWaPvPshH_ebgzmeOBeN6ufCdTca1g&s',
+//   },
+//   {
+//     id: 5,
+//     name: 'Ambon',
+//     lat: -3.69583,
+//     lon: 128.18333,
+//     nickname: null,
+//     image:
+//       'https://www.indonesia.travel/content/dam/indtravelrevamp/id-id/destinasi/ambon/thumbnail.jpg',
+//   },
+//   {
+//     id: 6,
+//     name: 'Samarinda',
+//     lat: -0.502106,
+//     lon: 117.153709,
+//     nickname: null,
+//     image:
+//       'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTjL8bGvP4rjn5YJeGKzTyiiioFA9pweMohfQ&s',
+//   },
+//   {
+//     id: 7,
+//     name: 'Pontianak',
+//     lat: 0.0,
+//     lon: 109.333336,
+//     nickname: null,
+//     image:
+//       'https://upload.wikimedia.org/wikipedia/commons/1/1f/Equator_Pontianak_Monument.jpg',
+//   },
+//   {
+//     id: 8,
+//     name: 'Medan',
+//     lat: 3.597031,
+//     lon: 98.678513,
+//     nickname: null,
+//     image:
+//       'https://www.rumah123.com/seo-cms/assets/Medan_Kota_c5625f2988/Medan_Kota_c5625f2988.webp',
+//   },
+//   {
+//     id: 9,
+//     name: 'Kupang',
+//     lat: -10.178757,
+//     lon: 123.597603,
+//     nickname: null,
+//     image:
+//       'https://upload.wikimedia.org/wikipedia/commons/thumb/4/43/Night_at_Kupang_city.jpg/1200px-Night_at_Kupang_city.jpg',
+//   },
+//   {
+//     id: 10,
+//     name: 'Jayapura',
+//     lat: -2.53371,
+//     lon: 140.71813,
+//     nickname: null,
+//     image:
+//       'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQqIm1MywLu2mg-Mcn-1bex5-bScXbtFxijPA&s',
+//   },
+//   {
+//     id: 11,
+//     name: 'Banjarbaru',
+//     lat: -3.457242,
+//     lon: 114.810318,
+//     nickname: null,
+//     image:
+//       'https://upload.wikimedia.org/wikipedia/commons/2/2b/Al_Munawarah_Grand_Mosque_Banjarbaru.jpg',
+//   },
+//   {
+//     id: 12,
+//     name: 'Palu',
+//     lat: -0.8989404,
+//     lon: 119.8714135,
+//     nickname: null,
+//     image:
+//       'https://upload.wikimedia.org/wikipedia/commons/1/1c/Palu_city_at_night.png',
+//   },
+//   {
+//     id: 13,
+//     name: 'Manado',
+//     lat: 1.47483,
+//     lon: 124.842079,
+//     nickname: null,
+//     image:
+//       'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRqGyrwkfM9Ktc-B1GST6AASOuq4yA1xrRbVw&s',
+//   },
+//   {
+//     id: 14,
+//     name: 'Yogyakarta',
+//     lat: -7.797068,
+//     lon: 110.370529,
+//     nickname: 'Kota Pelajar',
+//     image:
+//       'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRYlFPTmiGqBY5YxA4qat0eg05iHHaUp22w1A&s',
+//   },
+//   {
+//     id: 15,
+//     name: 'Malang',
+//     lat: -7.983908,
+//     lon: 112.621391,
+//     nickname: null,
+//     image:
+//       'https://upload.wikimedia.org/wikipedia/commons/thumb/9/96/Tugu_Malang.jpg/288px-Tugu_Malang.jpg',
+//   },
+//   {
+//     id: 16,
+//     name: 'Jakarta',
+//     lat: -6.2,
+//     lon: 106.816666,
+//     nickname: 'Kota Metropolitan',
+//     image:
+//       'https://pict.sindonews.net/dyn/850/pena/news/2021/02/26/173/348284/beragam-nama-jakarta-sejak-tahun-397-sampai-sekarang-txm.jpg',
+//   },
+// ];
+
+// export const provinsiData = [
+//   {
+//     id: 1,
+//     name: 'Aceh',
+//     symbolURL:
+//       'https://upload.wikimedia.org/wikipedia/commons/thumb/4/41/Coat_of_arms_of_Aceh.svg/1200px-Coat_of_arms_of_Aceh.svg.png',
+//   },
+//   {
+//     id: 2,
+//     name: 'Jawa Tengah',
+//     symbolURL:
+//       'https://upload.wikimedia.org/wikipedia/commons/thumb/b/bd/Coat_of_arms_of_Central_Java.svg/220px-Coat_of_arms_of_Central_Java.svg.png',
+//   },
+// ];
+
+export const data = {
+  provinces: [
+    {
+      id: 1,
+      name: 'Aceh',
+      slug: 'aceh',
+      logoUrl:
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/4/41/Coat_of_arms_of_Aceh.svg/1200px-Coat_of_arms_of_Aceh.svg.png',
+      coordinates: {
+        latitude: 4.695135,
+        longitude: 96.749397,
+      },
+      generalInfo: {
+        area: '57,956 km²',
+        population: '5,274,871',
+        capital: 'Banda Aceh',
+        statistics: {
+          density: '91 per km²',
+          otherStat: 'example value',
+        },
+      },
+      geography: {
+        description:
+          'Aceh is located on the northern tip of Sumatra...',
+        climate: 'Tropical rainforest',
+        topography: 'Mountainous areas and coastal plains',
+      },
+      cultureAndTraditions: {
+        traditionalFood: [
+          {
+            name: 'Mie Aceh',
+            description: 'Spicy noodle dish',
+            imageUrl:
+              'https://example.com/images/mie_aceh.jpg',
+          },
+          {
+            name: 'Roti Canai',
+            description: 'Flatbread served with curry',
+            imageUrl:
+              'https://example.com/images/roti_canai.jpg',
+          },
+        ],
+        languages: ['Indonesian', 'Acehnese'],
+        localFestivals: [
+          {
+            name: 'Festival Seudati',
+            date: 'August',
+            description: 'Traditional dance performance',
+          },
+        ],
+      },
+      tourism: {
+        destinations: [
+          {
+            name: 'Sabang Beach',
+            description: 'Beautiful white sand beach',
+            imageUrl:
+              'https://example.com/images/sabang_beach.jpg',
+          },
+          {
+            name: 'Baiturrahman Grand Mosque',
+            description:
+              'Historic mosque with stunning architecture',
+            imageUrl:
+              'https://example.com/images/baiturrahman_mosque.jpg',
+          },
+        ],
+      },
+      economy: {
+        sectors: [
+          {
+            name: 'Agriculture',
+            description:
+              'Mainly palm oil, coffee, and rice production',
+          },
+          {
+            name: 'Fishing',
+            description:
+              'Aceh is known for its fishing industry',
+          },
+        ],
+      },
+      publicFacilities: {
+        education: [
+          {
+            name: 'University of Syiah Kuala',
+            type: 'University',
+            description: 'Leading university in Banda Aceh',
+          },
+        ],
+        healthcare: [
+          {
+            name: 'RSUD Zainoel Abidin',
+            type: 'Hospital',
+            description: 'Main public hospital in Aceh',
+          },
+        ],
+        transportation: [
+          'Banda Aceh Airport',
+          'Ferries to Sabang Island',
+        ],
+      },
+      photoGallery: [
+        'https://example.com/images/gallery1.jpg',
+        'https://example.com/images/gallery2.jpg',
+        'https://example.com/images/gallery3.jpg',
+      ],
+      funFacts: [
+        "Aceh is known as the 'Veranda of Mecca' due to its Islamic heritage.",
+        'It is one of the most religiously conservative regions in Indonesia.',
+      ],
+    },
+    {
+      id: 14,
+      name: 'North Sumatra',
+      slug: 'north-sumatra',
+      logoUrl:
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c8/Coat_of_arms_of_North_Sumatra.svg/800px-Coat_of_arms_of_North_Sumatra.svg.png',
+      coordinates: {
+        latitude: 2.115354,
+        longitude: 99.545097,
+      },
+      generalInfo: {
+        area: '72,981 km²',
+        population: '14,799,361',
+        capital: 'Medan',
+        statistics: {
+          density: '202 per km²',
+          otherStat: 'example value',
+        },
+      },
+      geography: {
+        description:
+          'North Sumatra is located on the island of Sumatra...',
+        climate: 'Tropical',
+        topography:
+          'Highlands, volcanoes, and forested areas',
+      },
+      cultureAndTraditions: {
+        traditionalFood: [
+          {
+            name: 'Saksang',
+            description: 'Spicy pork or beef stew',
+            imageUrl:
+              'https://example.com/images/saksang.jpg',
+          },
+        ],
+        languages: ['Indonesian', 'Batak'],
+        localFestivals: [
+          {
+            name: 'Lake Toba Festival',
+            date: 'September',
+            description:
+              'Celebration of Batak culture around Lake Toba',
+          },
+        ],
+      },
+      tourism: {
+        destinations: [
+          {
+            name: 'Lake Toba',
+            description:
+              'The largest volcanic lake in the world',
+            imageUrl:
+              'https://example.com/images/lake_toba.jpg',
+          },
+        ],
+      },
+      economy: {
+        sectors: [
+          {
+            name: 'Agriculture',
+            description:
+              'Rubber, coffee, and palm oil production',
+          },
+        ],
+      },
+      publicFacilities: {
+        education: [
+          {
+            name: 'University of North Sumatra',
+            type: 'University',
+            description: 'Leading university in Medan',
+          },
+        ],
+        healthcare: [
+          {
+            name: 'RSUP H. Adam Malik',
+            type: 'Hospital',
+            description: 'Main public hospital in Medan',
+          },
+        ],
+        transportation: [
+          'Kualanamu International Airport',
+          'Trains to Jakarta',
+        ],
+      },
+      photoGallery: [
+        'https://example.com/images/toba_gallery1.jpg',
+        'https://example.com/images/toba_gallery2.jpg',
+      ],
+      funFacts: [
+        'Lake Toba is over 70,000 years old and is the result of a supervolcanic eruption.',
+      ],
+    },
+  ],
+};
 
 export const topology = await fetch(
   'https://code.highcharts.com/mapdata/countries/id/id-all.topo.json',
